7e7708f3f54b7deca314e315f0a5ff97b693778c
JIGC1 <JIGC1@PROTONMAIL.COM>
2022-05-05 01:27:58 -0500

    more js changes

--- home.js
+++ home.js
@@ -114,3 +114,103 @@ console.log(fruit.toLowerCase()); // lower case
 console.log(fruit.charAt(2)); // gives character at given position
 console.log(fruit[2]); // same as charAt
 console.log(fruit.split('')) // splits by given character '' will split all characters
+
+// Arrays
+let fruits = ['apple', 'banana', 'orange', 'pineapples'];
+fruits = new Array ('apple', 'banana', 'orange', 'pineapples'); // also an array just different method
+
+console.log(fruits[2]); // access value at index 2nd
+
+fruits[0] = 'pear';
+console.log(fruits);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// array common methods
+console.log('to string', fruits.toString()); // convert array to string and print
+console.log(fruits.join(' - ')); // joins each of the values in the array with a given character
+console.log(fruits, fruits.pop(), fruits); // prints array, removes last item, prints array
+console.log(fruits, fruits.push('pineapples'), fruits); // prints array, adds item, prints array
+console.log(fruits[3]); // prints value at 3rd
+fruits[3] = 'mango'; // same as push
+console.log (fruits)
+console.log(fruits.shift());  // remove first element from an array
+console.log(fruits);
+console.log(fruits.unshift('apple')); // add first element to an array
+console.log(fruits);
+
+let vegetables = ['asparagus', 'tomato', 'broccoli'];
+let allGroceries = fruits.concat(vegetables); // combine arrays
+console.log(allGroceries);
+console.log(allGroceries.slice(1, 4));
+console.log(allGroceries.reverse());
+console.log(allGroceries.sort());
+
+let someNumbers = [5,10,2,49,85,733,900];
+console.log(someNumbers.sort(function (a, b) {return a-b})); // sort ascending order
+console.log(someNumbers.sort(function (a, b) {return b-a})); // sort descending order
+
+let emptyArray = new Array ();
+for (let num = 0; num <=10; num++) {
+    emptyArray.push(num);
+}
+console.log(emptyArray)
+
+// objects in Javascript
+// also known as dictionaries in Python
+
+student = {
+    first: 'John', 
+    last: 'doe', 
+    age: '29', 
+    height: "6'",
+    studentInfo: function (){
+        return this.first + '\n' + this.last + '\n' + this.age;
+    }
+};
+
+console.log(student.first);
+console.log(student.last);
+student.first = 'Jane'; // change value
+student.age++;
+console.log(student.age);
+console.log(student.studentInfo());
+console.log(student.height);
+
+// Conditionals, Control Flows (if else)
+// 19-35 is my target demographic
+// && AND
+// || OR
+
+var age = '45' // prompt('What is your age'); //var is used whenever a prompt is going to be used
+if ((age >= 18) && (age <= 35)) {
+    status = 'target demo';
+    console.log(status);
+} else {
+    status = 'not my audience';
+    console.log(status);
+}
+
+// Switch statements
+// Differentiate between weekday vs weekend
+// day 0 --> Sunday
+// day 6 --> Saturday
+// day 4 --> Thursday --> weekday
+
+switch (6) {
+    case 0:
+        Text = 'weekend';
+        break;
+    case 5:
+        Text = 'weekend';
+        break;
+    case 6:
+        Text = 'weekend';
+        break;
+    default:
+        Text = 'weekday';
+}
+
+console.log(Text);
